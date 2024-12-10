72a1075a9fd39e79f4b2ff3ff4ee656aec3bad74
DotesMcGoats <120683250+DotesMcGoats@users.noreply.github.com>
2024-12-10 20:45:47 +0000

    Switched to the OS module for Python and reworked the ping command accordingly.

--- ping.py
+++ ping.py
@@ -1,14 +1,18 @@
-import subprocess
+import os
 
 
-def ping(host):
-    output = []
-    try:
-        for i in host:
-            print(f"Pinging {i}")
-            output = subprocess.check_output(
-                ["ping", "-c", "1", i], universal_newlines=True
-            )
-        return output
-    except subprocess.CalledProcessError as e:
-        return f"Ping failed: {e}"
+def ping(hosts):
+    param = '-n' if os.sys.platform.lower() == 'win32' else '-c'
+    successful_hosts = []
+    
+    for host in hosts:
+        print(f"Pinging {host}")
+        response = os.system(f"ping {param} 1 {host}")
+        
+        if response == 0:
+            print(f"{host} is up!")
+            successful_hosts.append(host)
+        else:
+            print(f"{host} is down!")
+    
+    return successful_hosts
