abbbb01ea3df5b1b9b6b314cae4be384c0d9017b
DotesMcGoats <120683250+DotesMcGoats@users.noreply.github.com>
2024-12-14 14:20:56 -0500

    Added timer and logging functions.

--- ping.py
+++ ping.py
@@ -1,27 +1,26 @@
 import os
-import time
+from decorators import timer, logger
 
 
-def ping(ip):
+def update_progress(formatted_time, i, total_ips):
+    print(f"\r{formatted_time}; {i}/{total_ips} IPs scanned", end="")
+
+@logger
+@timer(lambda formatted_time, i, total_ips: update_progress(formatted_time, i, total_ips))
+def ping(state, ip):
     param = '-n' if os.sys.platform.lower() == 'win32' else '-c'
-    successful_hosts = []
     null_device = 'NUL' if os.sys.platform.lower() == 'win32' else '/dev/null'
-    total_ips = len(ip)
-    start_time = time.time()  # Start time of the scan
-    last_update = start_time
+    successful_hosts = []
+    state['total_ips'] = len(ip)
 
     for i, ip in enumerate(ip, start=1):
         response = os.system(f"ping {param} 1 {ip} > {null_device} 2>&1")
 
         if response == 0:
             successful_hosts.append(ip)
-
-        # Updates the how much time has passed and how many IPs have been pinged every second
-        current_time = time.time()
-        if current_time - last_update >= 1:
-            elapsed_time = time.time() - start_time  # Calculate elapsed time
-            formatted_time = time.strftime("%H:%M:%S", time.gmtime(elapsed_time)) # Format elapsed time
-            print(f"\r{formatted_time}; {i}/{total_ips} IPs scanned", end="")
-            last_update = current_time
+        
+        # Update the progress after each ping
+        state['i'] = i
+        update_progress(state['formatted_time'], i, state['total_ips'])  # Update progress with current values
     
     return successful_hosts
