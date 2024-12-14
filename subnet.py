419df6621e26159042e9cc831251bb75b3ee9880
DotesMcGoats <120683250+DotesMcGoats@users.noreply.github.com>
2024-12-13 21:47:18 -0500

    Updated the function call to include .255.

--- subnet.py
+++ subnet.py
@@ -12,117 +12,125 @@ def cidr(ipAddr):
     oct4 = [oct4]
     match cidr:
         case 0:
-            oct1 = ipGenerator.ipGen(0, 255)
-            oct2 = ipGenerator.ipGen(oct2, 255)
-            oct3 = ipGenerator.ipGen(oct3, 255)
-            oct4 = ipGenerator.ipGen(oct4, 255)
+            oct1 = ipGenerator.ipGen(0, 256)
+            oct2 = ipGenerator.ipGen(oct2, 256)
+            oct3 = ipGenerator.ipGen(oct3, 256)
+            oct4 = ipGenerator.ipGen(oct4, 256)
         case 1:
             oct1 = ipGenerator.ipGen(oct1, 128)
-            oct2 = ipGenerator.ipGen(oct2, 255)
-            oct3 = ipGenerator.ipGen(oct3, 255)
-            oct4 = ipGenerator.ipGen(oct4, 255)
+            oct2 = ipGenerator.ipGen(oct2, 256)
+            oct3 = ipGenerator.ipGen(oct3, 256)
+            oct4 = ipGenerator.ipGen(oct4, 256)
         case 2:
             oct1 = ipGenerator.ipGen(oct1, 64)
-            oct2 = ipGenerator.ipGen(oct2, 255)
-            oct3 = ipGenerator.ipGen(oct3, 255)
-            oct4 = ipGenerator.ipGen(oct4, 255)
+            oct2 = ipGenerator.ipGen(oct2, 256)
+            oct3 = ipGenerator.ipGen(oct3, 256)
+            oct4 = ipGenerator.ipGen(oct4, 256)
         case 3:
             oct1 = ipGenerator.ipGen(oct1, 32)
-            oct2 = ipGenerator.ipGen(oct2, 255)
-            oct3 = ipGenerator.ipGen(oct3, 255)
-            oct4 = ipGenerator.ipGen(oct4, 255)
+            oct2 = ipGenerator.ipGen(oct2, 256)
+            oct3 = ipGenerator.ipGen(oct3, 256)
+            oct4 = ipGenerator.ipGen(oct4, 256)
         case 4:
             oct1 = ipGenerator.ipGen(oct1, 16)
-            oct2 = ipGenerator.ipGen(oct2, 255)
-            oct3 = ipGenerator.ipGen(oct3, 255)
-            oct4 = ipGenerator.ipGen(oct4, 255)
+            oct2 = ipGenerator.ipGen(oct2, 256)
+            oct3 = ipGenerator.ipGen(oct3, 256)
+            oct4 = ipGenerator.ipGen(oct4, 256)
         case 5:
             oct1 = ipGenerator.ipGen(oct1, 8)
-            oct2 = ipGenerator.ipGen(oct2, 255)
-            oct3 = ipGenerator.ipGen(oct3, 255)
-            oct4 = ipGenerator.ipGen(oct4, 255)
+            oct2 = ipGenerator.ipGen(oct2, 256)
+            oct3 = ipGenerator.ipGen(oct3, 256)
+            oct4 = ipGenerator.ipGen(oct4, 256)
         case 6:
             oct1 = ipGenerator.ipGen(oct1, 4)
-            oct2 = ipGenerator.ipGen(oct2, 255)
-            oct3 = ipGenerator.ipGen(oct3, 255)
-            oct4 = ipGenerator.ipGen(oct4, 255)
+            oct2 = ipGenerator.ipGen(oct2, 256)
+            oct3 = ipGenerator.ipGen(oct3, 256)
+            oct4 = ipGenerator.ipGen(oct4, 256)
         case 7:
             oct1 = ipGenerator.ipGen(oct1, 2)
-            oct2 = ipGenerator.ipGen(oct2, 255)
-            oct3 = ipGenerator.ipGen(oct3, 255)
-            oct4 = ipGenerator.ipGen(oct4, 255)
+            oct2 = ipGenerator.ipGen(oct2, 256)
+            oct3 = ipGenerator.ipGen(oct3, 256)
+            oct4 = ipGenerator.ipGen(oct4, 256)
         case 8:
-            oct2 = ipGenerator.ipGen(oct2, 255)
-            oct3 = ipGenerator.ipGen(oct3, 255)
-            oct4 = ipGenerator.ipGen(oct4, 255)
+            oct2 = ipGenerator.ipGen(oct2, 256)
+            oct3 = ipGenerator.ipGen(oct3, 256)
+            oct4 = ipGenerator.ipGen(oct4, 256)
         case 9:
             oct2 = ipGenerator.ipGen(oct2, 128)
-            oct3 = ipGenerator.ipGen(oct3, 255)
-            oct4 = ipGenerator.ipGen(oct4, 255)
+            oct3 = ipGenerator.ipGen(oct3, 256)
+            oct4 = ipGenerator.ipGen(oct4, 256)
         case 10:
             oct2 = ipGenerator.ipGen(oct2, 64)
-            oct3 = ipGenerator.ipGen(oct3, 255)
-            oct4 = ipGenerator.ipGen(oct4, 255)
+            oct3 = ipGenerator.ipGen(oct3, 256)
+            oct4 = ipGenerator.ipGen(oct4, 256)
         case 11:
             oct2 = ipGenerator.ipGen(oct2, 32)
-            oct3 = ipGenerator.ipGen(oct3, 255)
-            oct4 = ipGenerator.ipGen(oct4, 255)
+            oct3 = ipGenerator.ipGen(oct3, 256)
+            oct4 = ipGenerator.ipGen(oct4, 256)
         case 12:
             oct2 = ipGenerator.ipGen(oct2, 16)
-            oct3 = ipGenerator.ipGen(oct3, 255)
-            oct4 = ipGenerator.ipGen(oct4, 255)
+            oct3 = ipGenerator.ipGen(oct3, 256)
+            oct4 = ipGenerator.ipGen(oct4, 256)
         case 13:
             oct2 = ipGenerator.ipGen(oct2, 8)
-            oct3 = ipGenerator.ipGen(oct3, 255)
-            oct4 = ipGenerator.ipGen(oct4, 255)
+            oct3 = ipGenerator.ipGen(oct3, 256)
+            oct4 = ipGenerator.ipGen(oct4, 256)
         case 14:
             oct2 = ipGenerator.ipGen(oct2, 4)
-            oct3 = ipGenerator.ipGen(oct3, 255)
-            oct4 = ipGenerator.ipGen(oct4, 255)
+            oct3 = ipGenerator.ipGen(oct3, 256)
+            oct4 = ipGenerator.ipGen(oct4, 256)
         case 15:
             oct2 = ipGenerator.ipGen(oct2, 2)
-            oct3 = ipGenerator.ipGen(oct3, 255)
-            oct4 = ipGenerator.ipGen(oct4, 255)
+            oct3 = ipGenerator.ipGen(oct3, 256)
+            oct4 = ipGenerator.ipGen(oct4, 256)
         case 16:
-            oct3 = ipGenerator.ipGen(oct3, 255)
-            oct4 = ipGenerator.ipGen(oct4, 255)
+            oct3 = ipGenerator.ipGen(oct3, 256)
+            oct4 = ipGenerator.ipGen(oct4, 256)
         case 17:
             oct3 = ipGenerator.ipGen(oct3, 128)
-            oct4 = ipGenerator.ipGen(oct4, 255)
+            oct4 = ipGenerator.ipGen(oct4, 256)
         case 18:
             oct3 = ipGenerator.ipGen(oct3, 64)
-            oct4 = ipGenerator.ipGen(oct4, 255)
+            oct4 = ipGenerator.ipGen(oct4, 256)
         case 19:
             oct3 = ipGenerator.ipGen(oct3, 32)
-            oct4 = ipGenerator.ipGen(oct4, 255)
+            oct4 = ipGenerator.ipGen(oct4, 256)
         case 20:
             oct3 = ipGenerator.ipGen(oct3, 16)
-            oct4 = ipGenerator.ipGen(oct4, 255)
+            oct4 = ipGenerator.ipGen(oct4, 256)
         case 21:
             oct3 = ipGenerator.ipGen(oct3, 8)
-            oct4 = ipGenerator.ipGen(oct4, 255)
+            oct4 = ipGenerator.ipGen(oct4, 256)
         case 22:
             oct3 = ipGenerator.ipGen(oct3, 4)
-            oct4 = ipGenerator.ipGen(oct4, 255)
+            oct4 = ipGenerator.ipGen(oct4, 256)
         case 23:
             oct3 = ipGenerator.ipGen(oct3, 2)
-            oct4 = ipGenerator.ipGen(oct4, 255)
+            oct4 = ipGenerator.ipGen(oct4, 256)
         case 24:
-            oct4 = ipGenerator.ipGen(oct4, 255)
+            oct4 = ipGenerator.ipGen(oct4, 256)
+            print(oct4)
         case 25:
             oct4 = ipGenerator.ipGen(oct4, 128)
+            print(oct4)
         case 26:
             oct4 = ipGenerator.ipGen(oct4, 64)
+            print(oct4)
         case 27:
             oct4 = ipGenerator.ipGen(oct4, 32)
+            print(oct4)
         case 28:
             oct4 = ipGenerator.ipGen(oct4, 16)
+            print(oct4)
         case 29:
             oct4 = ipGenerator.ipGen(oct4, 8)
+            print(oct4)
         case 30:
             oct4 = ipGenerator.ipGen(oct4, 4)
+            print(oct4)
         case 31:
             oct4 = ipGenerator.ipGen(oct4, 2)
+            print(oct4)
         case 32:
             print("There's only one IP address in this subnet...stoopid.")
 
